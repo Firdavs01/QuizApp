49e9baaf4e4cf34138dfa53bf5dbbb4ae29b40bb
Firdavs <firdavsstandoff@gmail.com>
2026-03-22 19:10:03 +0500

    50% of the main code work

--- js/checkAnswer.js
+++ js/checkAnswer.js
@@ -2,23 +2,33 @@
 
 import { render } from "./render.js"
 import { questions } from "./api.js"
+import { containerElement } from "./elements.js"
 
 function checkAnswer(currentIndex) {
     const valueOfTheInput = document.querySelector(`input[name="question-${currentIndex}"]:checked`)
     const correctAnswer = questions[currentIndex].correct_answer
 
     if (valueOfTheInput) {
-        const value = valueOfTheInput.value
-
-        if (correctAnswer === value) {
-            alert("Правильно!")
-        } else {
-            alert(`Неправильно X Правильный ответ ${correctAnswer}`)
-        }
-    } else {
-        alert("choose your answer")
+        alert("Choose answer");
         return
     }
+
+    const value = valueOfTheInput.value
+
+    let winWindow = document.querySelector('.result-window')
+    if (!winWindow) {
+        winWindow = document.createElement('div')
+        winWindow.classList.add('result-window')
+        document.body.appendChild(winWindow)
+    }
+
+    if (correctAnswer === value) {
+        winWindow.innerHTML = `<h3>Congratulation you win!!!</h3>`
+    } else {
+        winWindow.innerHTML = `<h3>you lose</h3>`
+    }
+
+    containerElement.innerHTML = ``
     render(currentIndex + 1)
 }
 
